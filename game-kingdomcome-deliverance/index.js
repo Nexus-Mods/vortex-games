4bd4146f9e99cfc2c7b8a07eb5e5f64b06c4e701
TanninOne <sherb@gmx.net>
2018-03-16 13:57:12 +0100

    kcd: using new (1.3) modding mechanism

--- game-kingdomcome-deliverance/index.js
+++ game-kingdomcome-deliverance/index.js
@@ -1,53 +1,58 @@
+const Promise = require('bluebird');
 const { remote } = require('electron');
-const { fs, log, util } = require('vortex-api');
 const path = require('path');
+const { fs, log, selectors, util } = require('vortex-api');
 
 function findGame() {
   return util.steam.findByAppId('379430')
       .then(game => game.gamePath);
 }
 
-function test(game, discovery) {
-  if (game.id !== 'kingdomcomedeliverance') {
-    return undefined;
-  }
-
-  return {
-    baseFiles: [
-      {
-        in: path.join(discovery.path, 'user.cfg'),
-        out: 'user.cfg',
-      },
-    ],
-    filter: filePath => path.basename(filePath) === 'user.cfg',
-  };
+function prepareForModding(discovery) {
+  return fs.ensureDirAsync(path.join(discovery.path, 'Mods'));
 }
 
-function merge(filePath, mergeDir) {
-  return fs.readFileAsync(filePath)
-      .then(modData => fs.writeFileAsync(path.join(mergeDir, 'user.cfg'),
-                                         '\n' + modData, {flag: 'a'}));
+function transformId(input) {
+  // the game doesn't like spaces in its mod names
+  return input.replace(/[ -.]/g, '');
 }
-
 
 function main(context) {
   context.registerGame({
     id: 'kingdomcomedeliverance',
     name: 'Kingdom Come: Deliverance',
-    mergeMods: false,
+    mergeMods: mod => transformId(mod.id),
     queryPath: findGame,
-    queryModPath: () => '.',
+    queryModPath: () => 'Mods',
     logo: 'gameart.png',
     executable: () => 'Bin/Win64/KingdomCome.exe',
     requiredFiles: [
       'Bin/Win64/KingdomCome.exe',
     ],
+    setup: prepareForModding,
     details: {
       steamAppId: 379430,
     },
   });
 
-  context.registerMerge(test, merge, '');
+  context.once(() => {
+    // the bake-settings event receives the list of enabled mods, sorted by priority. perfect.
+    context.api.events.on('bake-settings', (gameId, mods) => {
+      if (gameId === 'kingdomcomedeliverance') {
+        const store = context.api.store;
+        const state = store.getState();
+        const discovery = util.getSafe(state, ['settings', 'gameMode', 'discovered', gameId], undefined);
+        console.log('discovery', discovery, gameId, mods);
+        if (discovery === undefined) {
+          // should never happen and if it does it will cause errors elsewhere as well
+          log('error', 'kingdomcomedeliverance was not discovered');
+          return;
+        }
+        fs.writeFileAsync(path.join(discovery.path, 'Mods', 'mod_order.txt'),
+          mods.map(mod => transformId(mod.id)).join('\n'));
+      }
+    });
+  })
 
   return true;
 }
