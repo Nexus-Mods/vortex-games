d81878956ec2f254119d5f4aec11b91e58dea4a0
Chloe Stars <c.stars@icloud.com>
2018-03-20 20:29:40 +1100

    Added support for Kerbal Space Program

---
+++ game-kerbalspaceprogram/index.js
@@ -0,0 +1,33 @@
+const Promise = require('bluebird');
+const { remote } = require('electron');
+const path = require('path');
+const { fs, log, selectors, util } = require('vortex-api');
+
+function findGame() {
+  return util.steam.findByAppId('220200')
+      .then(game => game.gamePath);
+}
+
+function main(context) {
+  context.registerGame({
+    id: 'kerbalspaceprogram',
+    name: 'Kerbal Space Program',
+    mergeMods: true,
+    queryPath: findGame,
+    queryModPath: () => 'GameData',
+    logo: 'gameart.png',
+    executable: () => 'KSP_x64.exe',
+    requiredFiles: [
+      'KSP_x64.exe',
+    ],
+    details: {
+      steamAppId: 220200,
+    },
+  });
+
+  return true;
+}
+
+module.exports = {
+  default: main,
+};
