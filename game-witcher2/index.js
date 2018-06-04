3ee57365710d6fbf8178067cf9add11a3e273133
TanninOne <sherb@gmx.net>
2018-06-04 19:32:22 +0200

    fixed: (witcher 2) exception when preparing for modding

--- game-witcher2/index.js
+++ game-witcher2/index.js
@@ -1,6 +1,6 @@
 const Promise = require('bluebird');
-const fs = require('fs');
 const path = require('path');
+const { fs } = require('vortex-api');
 const Registry = require('winreg');
 
 function findGame() {
