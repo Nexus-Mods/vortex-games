f8289812f158b81798041bbe3a266386510eeaaf
TanninOne <seppleviathan@gmx.de>
2019-03-05 13:42:34 +0100

    slight code cleanup

--- game-gardenpaws/index.js
+++ game-gardenpaws/index.js
@@ -12,96 +12,83 @@ const Name = 'Garden Paws';
 const ExeName = 'GardenPaws';
 const SteamId = 840010;
 
-function main(context) 
-{
-    context.registerGame(
-	{
-        id: NexusId,
-        name: Name,
-        logo: 'gameart.png',
-        mergeMods: true,
-        queryPath: findGame,
-        queryModPath: () => 'Mods',
-        executable: () => ExeName + '.exe',
-        requiredFiles: [ExeName + '.exe'],
-        details: 
-		{
-            steamAppId: SteamId,
-        },
-        setup: setup,
-        supportedTools: [
-            {
-                id: 'UnityModManager',
-                name: 'Unity Mod Manager',
-                logo: 'umm.png',
-                queryPath : findUnityModManager,
-                executable: () => 'UnityModManager.exe',
-                requiredFiles: ['UnityModManager.exe'],
-            }],
+function main(context) {
+  context.registerGame(
+    {
+      id: NexusId,
+      name: Name,
+      logo: 'gameart.png',
+      mergeMods: true,
+      queryPath: findGame,
+      queryModPath: () => 'Mods',
+      executable: () => ExeName + '.exe',
+      requiredFiles: [ExeName + '.exe'],
+      details:
+      {
+        steamAppId: SteamId,
+      },
+      setup: setup,
+      supportedTools: [
+        {
+          id: 'UnityModManager',
+          name: 'Unity Mod Manager',
+          logo: 'umm.png',
+          queryPath: findUnityModManager,
+          executable: () => 'UnityModManager.exe',
+          requiredFiles: ['UnityModManager.exe'],
+        }],
     });
 
-    function findGame() 
-	{
-        return util.steam.findByAppId(SteamId.toString()).then(game => game.gamePath);
-    }
+  function findGame() {
+    return util.steam.findByAppId(SteamId.toString()).then(game => game.gamePath);
+  }
 
-    function findUnityModManager() 
-	{
-        let result = '';
-        if (IsWin) 
-		{
-            try 
-			{
-                const path = winapi.RegGetValue('HKEY_CURRENT_USER', 'Software\\UnityModManager', 'Path');
-                if (path) 
-				{
-                    result = path.value;
-                }
-            } 
-			catch (err) 
-			{
-				
-            }
+  function findUnityModManager() {
+    let result = '';
+    if (IsWin) {
+      try {
+        const path = winapi.RegGetValue('HKEY_CURRENT_USER', 'Software\\UnityModManager', 'Path');
+        if (path) {
+          result = path.value;
         }
+      }
+      catch (err) {
 
-        return Promise.resolve(result);
+      }
     }
 
-    function setup(discovery) 
-	{	
-		if (!fs.existsSync(path.join(discovery.path, ExeName + '_Data', 'Managed', 'UnityModManager', 'UnityModManager.dll')))
-		{					
-			new Promise((resolve, reject) => 
-			{
-				context.api.store.dispatch(
-					actions.showDialog(
-						'question',
-						'Action required',
-						{message: 'You must install Unity Mod Manager to use mods with ' + Name + '.'},
-						[
-							{label: 'Cancel', action: () => reject(new util.UserCanceled())},
-							{
-								label: 'Go to the Unity Mod Manager page', action: () => 
-								{
-									opn('https://www.nexusmods.com/site/mods/21/').catch(err => undefined);
-									reject(new util.UserCanceled());
-								}
-							}
-						]
-					)
-				);
-			});
-		}
-		if (!fs.existsSync(path.join(discovery.path, 'Mods'))) 
-		{
-			fs.mkdirSync(path.join(discovery.path, 'Mods'));
-		}
-    }
+    return Promise.resolve(result);
+  }
 
-    return true;
+  function setup(discovery) {
+    if (!fs.existsSync(path.join(discovery.path, ExeName + '_Data', 'Managed', 'UnityModManager', 'UnityModManager.dll'))) {
+      new Promise((resolve, reject) => {
+        context.api.store.dispatch(
+          actions.showDialog(
+            'question',
+            'Action required',
+            { message: 'You must install Unity Mod Manager to use mods with ' + Name + '.' },
+            [
+              { label: 'Cancel', action: () => reject(new util.UserCanceled()) },
+              {
+                label: 'Go to the Unity Mod Manager page', action: () => {
+                  opn('https://www.nexusmods.com/site/mods/21/').catch(err => undefined);
+                  reject(new util.UserCanceled());
+                }
+              }
+            ]
+          )
+        );
+      });
+    }
+    if (!fs.existsSync(path.join(discovery.path, 'Mods'))) {
+      fs.mkdirSync(path.join(discovery.path, 'Mods'));
+    }
+  }
+
+  return true;
 }
 
-module.exports = 
-{
+module.exports = {
     default: main
 };
