ecee245e3f0b7ba96f10e4b5721e5a063bab3487
adrian <idc.dts@gmail.com>
2019-04-17 11:31:41 +0100

    Adding support for Sekiro

---
+++ game-sekiro/index.js
@@ -0,0 +1,92 @@
+const Promise = require('bluebird');
+const opn = require('opn');
+const path = require('path');
+const { actions, fs, util } = require('vortex-api');
+
+let _API;
+const GAME_ID = 'sekiro';
+const STEAM_ID = 814380;
+const DINPUT = 'dinput8.dll';
+const DCX_EXT = '.dcx';
+
+function findGame() {
+  return util.steam.findByAppId(STEAM_ID.toString())
+    .then(game => game.gamePath);
+}
+
+function prepareForModding(discovery) {
+  const modEngineDInput = path.join(discovery.path, DINPUT);
+  const showModEngineDialog = () => new Promise((resolve, reject) => {
+    _API.store.dispatch(actions.showDialog('question', 'Action required',
+      {
+        message: 'Sekiro requires "Sekiro Mod Engine" for mods to install and function correctly.\n' 
+               + 'Vortex is able to install Mod Engine automatically (as a mod) but please ensure it is enabled.\n'
+               + 'and deployed at all times.'
+      },
+      [
+        { label: 'Continue', action: () => resolve() },
+        { label: 'Go to Mod Engine Page', action: () => {
+            opn('https://www.nexusmods.com/sekiro/mods/6').catch(err => undefined);
+            resolve();
+        }},
+      ]));
+  });
+
+  // Check whether mod engine is installed.
+  return fs.ensureDirWritableAsync(path.join(discovery.path, 'mods', 'parts'))
+    .then(() => fs.statAsync(modEngineDInput)
+      .catch(err => (err.code === 'ENOENT')
+        ? showModEngineDialog()
+        : Promise.reject(err)));
+}
+
+function hasLooseParts(files) {
+  const dcxFiles = files.filter(file => file.endsWith(DCX_EXT));
+  return (dcxFiles.length > 0)
+    ? dcxFiles[0].indexOf(path.sep + 'parts' + path.sep) === -1
+    : false;
+}
+
+function installLooseMod(files, destinationPath) {
+  const dcxFiles = files.filter(file => file.endsWith(DCX_EXT));
+  const instructions = dcxFiles.map(file => {
+    return {
+      type: 'copy',
+      source: file,
+      destination: path.join('parts', path.basename(file)),
+    };
+  });
+
+  return Promise.resolve({ instructions });
+}
+
+function testLooseMod(files, gameId) {
+  return Promise.resolve({
+    supported: ((gameId === GAME_ID) && (hasLooseParts(files))),
+    requiredFiles: [],
+  });
+}
+
+function main(context) {
+  _API = context.api;
+  context.registerGame({
+    id: GAME_ID,
+    name: 'Sekiro',
+    logo: 'gameart.png',
+    mergeMods: true,
+    queryPath: findGame,
+    queryModPath: () => 'mods',
+    executable: () => 'Sekiro.exe',
+    requiredFiles: [],
+    details: {
+      steamAppId: STEAM_ID,
+    },
+    setup: prepareForModding,
+  });
+
+  context.registerInstaller('sek-loose-files', 25, testLooseMod, installLooseMod);
+}
+
+module.exports = {
+  default: main
+};
