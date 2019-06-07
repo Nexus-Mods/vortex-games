002fd59a01b0d258834a2922e1e4884dce8d339b
IDCs <idc.dts@gmail.com>
2019-06-07 14:34:28 +0100

    Improved Subnautica installation pattern as discussed with QMM guys

--- game-subnautica/index.js
+++ game-subnautica/index.js
@@ -1,14 +1,16 @@
 const Promise = require('bluebird');
 const opn = require('opn');
 const path = require('path');
+const rjson = require('relaxed-json');
 const { actions, fs, util } = require('vortex-api');
 
 const MOD_FILE = 'mod.json';
+const GAME_ID = 'subnautica';
 
 class Subnautica {
   constructor(context) {
     this.context = context;
-    this.id = 'subnautica';
+    this.id = GAME_ID;
     this.name = 'Subnautica';
     this.mergeMods = true;
     this.queryModPath = () => 'QMods';
@@ -65,39 +67,39 @@ class Subnautica {
   }
 }
 
-function getModName(modFilePath) {
-  return fs.readFileAsync(modFilePath, { encoding: 'utf-8' })
-    .then(data => {
-      const stripBOM = (data.charAt(0) === '\uFEFF')
-        ? data.substr(1)
-        : data;
-
-      try {
-        const modFile = JSON.parse(stripBOM);
-        return Promise.resolve(modFile.Id);
-      } catch (err) {
-        return Promise.reject(new util.DataInvalid('Failed to parse mod.json file.'));
-      }
-    });
+function getModName(destination, modFile) {
+  const modFolder = path.basename(path.dirname(modFile));
+  const modFilePath = path.join(destination, modFile);
+  return (modFolder !== '.')
+    ? Promise.resolve(modFolder)
+    : fs.readFileAsync(modFilePath, { encoding: 'utf-8' })
+      .then(data => {
+        try {
+          const modFile = rjson.parse(util.deBOM(data));
+          return Promise.resolve(modFile.Id);
+        } catch (err) {
+          return Promise.reject(new util.DataInvalid('Failed to parse mod.json file.'));
+        }
+      });
 }
 
 function testMod(files, gameId) {
   return Promise.resolve({
-    supported: ((gameId === 'subnautica')
-      && (files.find(file => file.endsWith(MOD_FILE)) !== undefined)),
+    supported: ((gameId === GAME_ID)
+      && (files.find(file => path.basename(file) === MOD_FILE) !== undefined)),
     requiredFiles: []
   });
 }
 
 function installMod(files, destinationPath) {
-  const modFile = files.find(file => file.endsWith(MOD_FILE));
+  const modFile = files.find(file => path.basename(file) === MOD_FILE);
   const idx = modFile.indexOf(MOD_FILE);
   const rootPath = path.dirname(modFile);
 
   const filtered = files.filter(file => (!file.endsWith(path.sep))
     && (file.indexOf(rootPath) !== -1));
 
-  return getModName(path.join(destinationPath, modFile))
+  return getModName(destinationPath, modFile)
     .then(modName => {
       return Promise.map(filtered, file => {
         return Promise.resolve({
