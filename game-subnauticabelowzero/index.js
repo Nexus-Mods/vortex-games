75be941327b3fb10ca8d708235410910ff89ff81
IDCs <idc.dts@gmail.com>
2019-06-07 15:05:40 +0100

    Improved Subnautica(belowzero) pattern as discussed with QMM guys

--- game-subnauticabelowzero/index.js
+++ game-subnauticabelowzero/index.js
@@ -1,19 +1,20 @@
-const fs = require('fs');
-const { promisify } = require('util');
 const Promise = require('bluebird');
 const opn = require('opn');
 const path = require('path');
-const { actions, util } = require('vortex-api');
+const rjson = require('relaxed-json');
+const { actions, fs, util } = require('vortex-api');
+
+const MOD_FILE = 'mod.json';
+const GAME_ID = 'subnauticabelowzero';
 
 class SubnauticaBelowZero {
   constructor(context) {
     this.context = context;
-    this.id = 'subnauticabelowzero';
+    this.id = GAME_ID;
     this.name = 'Subnautica: Below Zero';
     this.mergeMods = true;
     this.queryModPath = () => 'QMods';
-    this.supportedTools = [
-      {
+    this.supportedTools = [{
       id: 'qmods',
       name: 'QModManager',
       executable: () => 'QModManager.exe',
@@ -21,6 +22,7 @@ class SubnauticaBelowZero {
         'QModManager.exe',
       ],
       relative: true,
+      shell: true,
       }
     ];
     this.logo = 'gameart.png';
@@ -44,28 +46,14 @@ class SubnauticaBelowZero {
     return util.steam.findByAppId('848450')
       .then(game => game.gamePath);
   }
-
-  async getPathExistsAsync(path) {
-      try {
-       await promisify(fs.access)(path, fs.constants.R_OK);
-       return true;
-      }
-      catch(err) {
-        return false;
-      }
-  }
   
   async setup(discovery) {
     // skip if QModManager found
-    let qmodPath = path.join(discovery.path, 'SubnauticaZero_Data', 'Managed', 'QModManager.exe')
-    let qmodFound = await this.getPathExistsAsync(qmodPath);
-    if (qmodFound) {
-      return;
-    }
+    let qmodPath = path.join(discovery.path, 'SubnauticaZero_Data', 'Managed', 'QModManager.exe');
   
     // show need-QModManager dialogue
     var context = this.context;
-    return new Promise((resolve, reject) => {
+    return fs.statAsync(qmodPath).catch(() => new Promise((resolve, reject) => {
       context.api.store.dispatch(
         actions.showDialog(
           'question',
@@ -77,12 +65,58 @@ class SubnauticaBelowZero {
           ]
         )
       );
-    });
+    }));
   }
+}
+
+function getModName(destination, modFile) {
+  const modFolder = path.basename(path.dirname(modFile));
+  const modFilePath = path.join(destination, modFile);
+  return (modFolder !== '.')
+    ? Promise.resolve(modFolder)
+    : fs.readFileAsync(modFilePath, { encoding: 'utf-8' })
+      .then(data => {
+        try {
+          const modFile = rjson.parse(util.deBOM(data));
+          return Promise.resolve(modFile.Id);
+        } catch (err) {
+          return Promise.reject(new util.DataInvalid('Failed to parse mod.json file.'));
+        }
+      });
+}
+
+function testMod(files, gameId) {
+  return Promise.resolve({
+    supported: ((gameId === GAME_ID)
+      && (files.find(file => path.basename(file) === MOD_FILE) !== undefined)),
+    requiredFiles: []
+  });
+}
+
+function installMod(files, destinationPath) {
+  const modFile = files.find(file => path.basename(file) === MOD_FILE);
+  const idx = modFile.indexOf(MOD_FILE);
+  const rootPath = path.dirname(modFile);
+
+  const filtered = files.filter(file => (!file.endsWith(path.sep))
+    && (file.indexOf(rootPath) !== -1));
+
+  return getModName(destinationPath, modFile)
+    .then(modName => {
+      return Promise.map(filtered, file => {
+        return Promise.resolve({
+          type: 'copy',
+          source: file,
+          destination: path.join(modName, file.substr(idx)),
+        });
+      });
+    })
+    .then(instructions => Promise.resolve({ instructions }));
 }
 
 module.exports = {
   default: function(context) {
     context.registerGame(new SubnauticaBelowZero(context));
+    context.registerInstaller('subnautica-belowzero-mod', 25, testMod, installMod);
   }
 };
