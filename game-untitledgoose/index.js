dcc4cb64f0abda6e501ef070b581a27c814b434d
IDCs <idc.dts@gmail.com>
2019-10-14 13:44:49 +0100

    (UGG) adding support

---
+++ game-untitledgoose/index.js
@@ -0,0 +1,57 @@
+const Promise = require('bluebird');
+const path = require('path');
+const { fs, util } = require('vortex-api');
+const { runPatcher } = require('patcher-harmony');
+
+const DATAPATH = path.join('Untitled_Data', 'Managed')
+const ENTRY_POINT = 'GameManager::Awake';
+
+function requiresLauncher() {
+  return util.epicGamesLauncher.isGameInstalled('Flour')
+    .then(epic => epic
+      ? { launcher: 'epic', addInfo: 'Flour' }
+      : undefined);
+}
+
+function findGame() {
+  // Currently no way for us to tell if the goose game is
+  //  installed.
+  return null;
+}
+
+function modPath() {
+  return path.join(DATAPATH, 'VortexMods');
+}
+
+function prepareForModding(discovery) {
+  const absPath = path.join(discovery.path, DATAPATH);
+  return runPatcher(absPath, ENTRY_POINT, false)
+    .then(() => fs.ensureDirWritableAsync(modPath(), () => Promise.resolve()));
+}
+
+function main(context) {
+  context.registerGame({
+    id: 'untitledgoosegame',
+    name: 'Untitled Goose Game',
+    mergeMods: true,
+    queryPath: findGame,
+    queryModPath: modPath,
+    requiresLauncher,
+    logo: 'gameart.png',
+    executable: () => 'Untitled.exe',
+    requiredFiles: [
+      'Untitled.exe',
+      'UnityPlayer.dll',
+    ],
+    setup: prepareForModding,
+    details: {
+      steamAppId: 427520,
+    },
+  });
+
+  return true;
+}
+
+module.exports = {
+  default: main,
+};
