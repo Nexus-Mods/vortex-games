2faebf2ae5fd93f1381f18c2af696c9235950d1e
IDCs <idc.dts@gmail.com>
2019-11-20 11:12:29 +0000

    (UGG) Adding harmony patcher toolbar buttons

--- game-untitledgoose/index.js
+++ game-untitledgoose/index.js
@@ -1,11 +1,13 @@
 const Promise = require('bluebird');
 const path = require('path');
-const { fs, util } = require('vortex-api');
+const { fs, log, selectors, util } = require('vortex-api');
 const { runPatcher } = require('harmony-patcher');
 
 const DATAPATH = path.join('Untitled_Data', 'Managed')
 const ENTRY_POINT = 'GameManager::Awake';
 const EPIC_APP_ID = 'Flour';
+
+const GAME_ID = 'untitledgoosegame';
 
 function requiresLauncher() {
   return util.epicGamesLauncher.isGameInstalled(EPIC_APP_ID)
@@ -23,6 +25,16 @@ function modPath() {
   return path.join(DATAPATH, 'VortexMods');
 }
 
+function getDiscoveryPath(state) {
+  const discovery = util.getSafe(state, ['settings', 'gameMode', 'discovered', GAME_ID], undefined);
+  if ((discovery === undefined) || (discovery.path === undefined)) {
+    log('error', 'untitledgoosegame was not discovered');
+    return undefined;
+  }
+
+  return discovery.path;
+}
+
 function prepareForModding(discovery) {
   const absPath = path.join(discovery.path, DATAPATH);
   return runPatcher(__dirname, absPath, ENTRY_POINT, false)
@@ -31,7 +43,7 @@ function prepareForModding(discovery) {
 
 function main(context) {
   context.registerGame({
-    id: 'untitledgoosegame',
+    id: GAME_ID,
     name: 'Untitled Goose Game',
     mergeMods: true,
     queryPath: findGame,
@@ -44,6 +56,46 @@ function main(context) {
       'UnityPlayer.dll',
     ],
     setup: prepareForModding,
+  });
+
+  const reportPatcherError = (err) => {
+    context.api.showErrorNotification('Patcher encountered errors',
+      'The patcher was unable to finish its operation "{{errorMsg}}"',
+      { replace: { errorMsg: err } });
+  };
+
+  context.registerAction('mod-icons', 500, 'savegame', {}, 'Patcher - Remove', () => {
+    const store = context.api.store;
+    const state = store.getState();
+    const gameMode = selectors.activeGameId(state);
+    if (gameMode !== GAME_ID) {
+      return false;
+    }
+    const dataPath = path.join(getDiscoveryPath(state), DATAPATH);
+    runPatcher(__dirname, dataPath, ENTRY_POINT, true)
+      .catch(err => reportPatcherError(err));
+    return true;
+  }, () => {
+    const state = context.api.store.getState();
+    const gameMode = selectors.activeGameId(state);
+    return (gameMode === GAME_ID)
+  });
+
+  context.registerAction('mod-icons', 500, 'savegame', {}, 'Patcher - Add', () => {
+    const store = context.api.store;
+    const state = store.getState();
+    const gameMode = selectors.activeGameId(state);
+    if (gameMode !== GAME_ID) {
+      return false;
+    }
+    const dataPath = path.join(getDiscoveryPath(state), DATAPATH);
+    runPatcher(__dirname, dataPath, ENTRY_POINT, false)
+      .catch(err => reportPatcherError(err));
+    return true;
+  }, () => {
+    const state = context.api.store.getState();
+    const gameMode = selectors.activeGameId(state);
+    return (gameMode === GAME_ID)
   });
 
   return true;
