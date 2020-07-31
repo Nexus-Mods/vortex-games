e35746c56317fb43dbfc43f5b56303bbe498bd67
idcz <idc.dts@gmail.com>
2020-07-31 09:50:33 +0100

    W3: Script Merger archive and executable are now integrity checked
    closes nexus-mods/vortex#7017

--- game-witcher3/common.js
+++ game-witcher3/common.js
@@ -1,3 +1,51 @@
+const crypto = require('crypto');
+const { fs } = require('vortex-api');
+
+class MD5ComparisonError extends Error {
+  constructor(message, file) {
+    super(message);
+    this.path = file;
+  }
+
+  get affectedFile() {
+    return this.path;
+  }
+
+  get errorMessage() {
+    return message + ': ' + this.path;
+  }
+}
+
+function calcHashImpl(filePath) {
+  return new Promise((resolve, reject) => {
+    const hash = crypto.createHash('md5');
+    const stream = fs.createReadStream(filePath);
+    stream.on('readable', () => {
+      const data = stream.read();
+      if (data) {
+        hash.update(data);
+      }
+    });
+    stream.on('end', () => resolve(hash.digest('hex')));
+    stream.on('error', reject);
+  });
+}
+
+function calcHash(filePath, tries = 3) {
+  return calcHashImpl(filePath)
+    .catch(err => {
+      if (['EMFILE', 'EBADF'].includes(err['code']) && (tries > 0)) {
+        return calcHash(filePath, tries - 1);
+      } else {
+        return Promise.reject(err);
+      }
+    });
+}
+
+exports.MD5ComparisonError = MD5ComparisonError;
+
+exports.getHash = calcHash;
+
 exports.GAME_ID = 'witcher3';
 
 // File used by some mods to define hotkey/input mapping
