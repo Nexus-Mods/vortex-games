5b611e77345bf3fe0ad6e0cd43d4e58230febecb
Mike <mike.watling@nexusmods.com>
2020-08-11 20:48:32 +0100

    (Morrowind) Added GOG detection and tools
    - Added GOG detection
    - Added TES3Edit and Construction Set to Tools

--- game-morrowind/index.js
+++ game-morrowind/index.js
@@ -2,28 +2,48 @@ const Promise = require('bluebird');
 const { util } = require('vortex-api');
 const winapi = require('winapi-bindings');
 
+const MORROWIND_ID = 'morrowind';
+const STEAMAPP_ID = '1172380';
+const GOGAPP_ID = '1435828767';
+
 function findGame() {
-  try {
-    const instPath = winapi.RegGetValue(
-      'HKEY_LOCAL_MACHINE',
-      'Software\\Wow6432Node\\Bethesda Softworks\\Morrowind',
-      'Installed Path');
-    if (!instPath) {
-      throw new Error('empty registry key');
-    }
-    return Promise.resolve(instPath.value);
-  } catch (err) {
-    return util.steam.findByName('The Elder Scrolls III: Morrowind')
-      .then(game => game.gamePath);
-  }
+  return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID])
+      .then(game => game.gamePath)
+      .catch(() => {
+        const instPath = winapi.RegGetValue(
+          'HKEY_LOCAL_MACHINE',
+          'Software\\Wow6432Node\\Bethesda Softworks\\Morrowind',
+          'Installed Path'
+        );
+
+        if (!instPath) throw new Error('empty registry key');
+        return Promise.resolve(instPath.value);
+      });
 }
 
-let tools = [
+const tools = [
+  {
+    id: 'tes3edit',
+    name: 'TES3Edit',
+    logo: 'tes3edit.png',
+    executable: () => 'TES3Edit.exe',
+    requiredFiles: []
+  },
+  {
+    id: 'mw-construction-set',
+    name: 'Construction Set',
+    executable: () => 'TES Construction Set.exe',
+    requiredFiles: [
+      'TES Construction Set.exe',
+    ],
+    relative: true,
+    exclusive: true
+  }
 ];
 
 function main(context) {
   context.registerGame({
-    id: 'morrowind',
+    id: MORROWIND_ID,
     name: 'Morrowind',
     mergeMods: true,
     queryPath: findGame,
@@ -35,10 +55,11 @@ function main(context) {
       'morrowind.exe',
     ],
     environment: {
-      SteamAPPId: '22320',
+      SteamAPPId: STEAMAPP_ID,
     },
     details: {
-      steamAppId: 22320,
+      steamAppId: parseInt(STEAMAPP_ID),
+      gogAppId: GOGAPP_ID
     },
   });
   return true;
