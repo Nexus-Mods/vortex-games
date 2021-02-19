bd2a922989ea37652b230bb4adaa0a7826099e18
idcs <idc.dts@gmail.com>
2021-02-19 10:21:57 +0000

    W3: Added ability to sort load order entries by deploy order/dep rules
    closes nexus-mods/vortex#8515

--- game-witcher3/index.js
+++ game-witcher3/index.js
@@ -21,7 +21,7 @@ const appUni = app || remote.app;
 const I18N_NAMESPACE = 'game-witcher3';
 
 const UNI_PATCH = 'mod0000____CompilationTrigger';
-const LOCKED_PREFIX = 'mod0000__';
+const LOCKED_PREFIX = 'mod0000_';
 
 const GOG_ID = '1207664663';
 const GOG_ID_GOTY = '1495134320';
@@ -1153,10 +1153,10 @@ function main(context) {
         bbcode: context.api.translate('This action will revert all manually set priorities and will re-instate priorities in an incremental ' 
           + 'manner starting from 1. Are you sure you want to do this ?', { ns: I18N_NAMESPACE }),
       }, [
-        { label: 'No', action: () => {
+        { label: 'Cancel', action: () => {
           return;
         }},
-        { label: 'Yes', action: () => {
+        { label: 'Reset Priorities', action: () => {
           const state = context.api.store.getState();
           const profile = selectors.activeProfile(state);
           const loadOrder = util.getSafe(state, ['persistent', 'loadOrder', profile.id], {});
@@ -1180,9 +1180,88 @@ function main(context) {
       return gameMode === GAME_ID;
     });
 
+  context.registerAction('generic-load-order-icons', 100, 'loot-sort', {}, 'Sort by Deploy Order',
+    () => {
+      context.api.showDialog('info', 'Sort by Deployment Order', {
+        bbcode: context.api.translate('This action will set priorities using the deployment rules '
+          + 'defined in the mods page. Are you sure you wish to proceed ?[br][/br][br][/br]'
+          + 'Please be aware that any externally added mods (added manually or by other tools) will be pushed '
+          + 'to the bottom of the list, while all mods that have been installed through Vortex will shift '
+          + 'in position to match the deploy order!', { ns: I18N_NAMESPACE }),
+      }, [
+        { label: 'Cancel', action: () => {
+          return;
+        }},
+        { label: 'Sort by Deploy Order', action: () => {
+          const state = context.api.getState();
+          const gameMods = state.persistent.mods[GAME_ID] || {};
+          const profile = selectors.activeProfile(state);
+          const mods = Object.keys(gameMods)
+            .filter(key => util.getSafe(profile, ['modState', key, 'enabled'], false))
+            .map(key => gameMods[key]);
+          return util.sortMods(GAME_ID, mods, context.api)
+            .then(sorted => {
+              const loadOrder = util.getSafe(state, ['persistent', 'loadOrder', profile.id], {});
+              const filtered = Object.keys(loadOrder).filter(key => sorted.find(mod => mod.id === key) !== undefined);
+              const manuallyAdded = Object.keys(loadOrder).filter(key => !filtered.includes(key));
+              const minimumIdx = manuallyAdded
+                .filter(key => key.includes(LOCKED_PREFIX))
+                .reduce((min, key) => {
+                  if (min <= loadOrder[key].pos) {
+                    min = loadOrder[key].pos + 1;
+                  }
+                  return min;
+                }, 0);
+              const manualLO = manuallyAdded.reduce((accum, key, idx) => {
+                if (key.includes(LOCKED_PREFIX)) {
+                  accum[key] = loadOrder[key];
+                  return accum;
+                }
+
+                const minimumPosition = (filtered.length + minimumIdx + 1);
+                if (loadOrder[key].pos < minimumPosition) {
+                  accum[key] = {
+                    ...loadOrder[key],
+                    pos: loadOrder[key].pos + (minimumPosition + idx),
+                    prefix: loadOrder[key].pos + (minimumPosition + idx + 1),
+                  };
+                  return accum;
+                } else {
+                  accum[key] = loadOrder[key];
+                  return accum;
+                }
+              }, {});
+              const newLO = filtered.reduce((accum, key) => {
+                const loEntry = loadOrder[key];
+                const idx = sorted.findIndex(mod => mod.id === key);
+                const assignedIdx = minimumIdx + idx;
+                accum[key] = {
+                  ...loEntry,
+                  pos: assignedIdx,
+                  prefix: assignedIdx + 1
+                };
+                return accum;
+              }, manualLO);
+
+              context.api.store.dispatch(actions.setLoadOrder(profile.id, newLO));
+              if (refreshFunc !== undefined) {
+                refreshFunc();
+              }
+            })
+            .catch(err => {
+              const allowReport = !(err instanceof util.CycleError);
+              context.api.showErrorNotification('Failed to sort by deployment order', err, { allowReport });
+            })
+        }},
+      ]);
+    }, () => {
+      const state = context.api.store.getState();
+      const gameMode = selectors.activeGameId(state);
+      return gameMode === GAME_ID;
+    });
   context.registerProfileFeature(
     'local_merges', 'boolean', 'settings', 'Profile Data',
-    'This profile will store and restore profile specific data (merged scripts, loadorder, etc)',
+    'This profile will store and restore profile specific data (merged scripts, loadorder, etc) when switching profiles',
     () => {
       const activeGameId = selectors.activeGameId(context.api.getState());
       return activeGameId === GAME_ID;
