91eb8439beda9d29a142722197415f38865ed424
idcs <idc.dts@gmail.com>
2021-03-15 10:18:50 +0000

    (Morrowind) added game discovery/launch through xbox store

--- game-morrowind/index.js
+++ game-morrowind/index.js
@@ -1,24 +1,35 @@
 const Promise = require('bluebird');
+const path = require('path');
 const { util } = require('vortex-api');
 const winapi = require('winapi-bindings');
 
 const MORROWIND_ID = 'morrowind';
 const STEAMAPP_ID = '22320';
 const GOGAPP_ID = '1435828767';
+const MS_ID = 'BethesdaSoftworks.TESMorrowind-PC';
 
+let _XBOX_PASS = false;
 function findGame() {
-  return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID])
-      .then(game => game.gamePath)
-      .catch(() => {
-        const instPath = winapi.RegGetValue(
-          'HKEY_LOCAL_MACHINE',
-          'Software\\Wow6432Node\\Bethesda Softworks\\Morrowind',
-          'Installed Path'
-        );
+  return util.GameStoreHelper.findByAppId([STEAMAPP_ID, GOGAPP_ID, MS_ID])
+    .then(game => {
+      _XBOX_PASS = game.gameStoreId === 'xbox';
+      // The xbox pass variant has a different file structure; we're naively
+      //  assuming that all XBOX copies (regardless of locale) will contain
+      //  the English variant as well (fingers crossed)
+      return (_XBOX_PASS)
+        ? Promise.resolve(path.join(game.gamePath, 'Morrowind GOTY English'))
+        : Promise.resolve(game.gamePath);
+    })
+    .catch(() => {
+      const instPath = winapi.RegGetValue(
+        'HKEY_LOCAL_MACHINE',
+        'Software\\Wow6432Node\\Bethesda Softworks\\Morrowind',
+        'Installed Path'
+      );
 
-        if (!instPath) throw new Error('empty registry key');
-        return Promise.resolve(instPath.value);
-      });
+      if (!instPath) throw new Error('empty registry key');
+      return Promise.resolve(instPath.value);
+    });
 }
 
 const tools = [
@@ -41,6 +52,20 @@ const tools = [
   }
 ];
 
+function requiresLauncher(gamePath) {
+  return (_XBOX_PASS)
+    ? Promise.resolve({
+      launcher: 'xbox',
+      addInfo: {
+        appId: MS_ID,
+        parameters: [
+          { appExecName: 'Game' },
+        ],
+      }
+    })
+    : Promise.resolve(undefined);
+}
+
 function main(context) {
   context.registerGame({
     id: MORROWIND_ID,
@@ -54,6 +79,7 @@ function main(context) {
     requiredFiles: [
       'morrowind.exe',
     ],
+    requiresLauncher,
     environment: {
       SteamAPPId: STEAMAPP_ID,
     },
