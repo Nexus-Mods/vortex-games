3e6f87c84dfbe67a3ee30024f6bd5335e81feda8
TanninOne <seppleviathan@gmx.de>
2023-02-02 10:23:50 +0100

    (SDV) updated setting location/styling

--- game-stardewvalley/Settings.tsx
+++ game-stardewvalley/Settings.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ControlLabel, FormGroup, HelpBlock } from 'react-bootstrap';
+import { ControlLabel, FormGroup, HelpBlock, Panel } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useSelector, useStore } from 'react-redux';
 import { Toggle } from 'vortex-api';
@@ -20,19 +20,23 @@ function Settings() {
   return (
     <form>
       <FormGroup controlId='default-enable'>
-        <ControlLabel>{t('Stardew Valley')}</ControlLabel>
-        <Toggle
-          checked={useRecommendations}
-          onToggle={setUseRecommendations}
-        >
-          {t('Use recommendations from the mod manifests')}
-        </Toggle>
-        <HelpBlock>
-          {t('If checked, when you install a mod for Stardew Valley you may get '
-             + 'suggestions for installing further mods, required or recommended by it.'
-             + 'This information could be wrong or incomplete so please carefully '
-             + 'consider before accepting them.')}
-        </HelpBlock>
+        <Panel>
+          <Panel.Body>
+            <ControlLabel>{t('Stardew Valley')}</ControlLabel>
+            <Toggle
+              checked={useRecommendations}
+              onToggle={setUseRecommendations}
+            >
+              {t('Use recommendations from the mod manifests')}
+            </Toggle>
+            <HelpBlock>
+              {t('If checked, when you install a mod for Stardew Valley you may get '
+                + 'suggestions for installing further mods, required or recommended by it.'
+                + 'This information could be wrong or incomplete so please carefully '
+                + 'consider before accepting them.')}
+            </HelpBlock>
+          </Panel.Body>
+        </Panel>
       </FormGroup>
     </form>
   );
