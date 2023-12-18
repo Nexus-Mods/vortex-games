6be1da2c7de580ea0b0c446f0f52ab39ca5a25a0
IDCs <idc.dts@gmail.com>
2023-12-18 10:51:05 +0000

    Adding SkyrimVR ESL support (#98)

--- game-skyrimvr/index.js
+++ game-skyrimvr/index.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
 const { getFileVersion, getFileVersionLocalized } = require('exe-version');
 const path = require('path');
-const { util } = require('vortex-api');
+const { actions, selectors, util } = require('vortex-api');
 const winapi = require('winapi-bindings');
+
+const GAME_ID = 'skyrimvr';
+const ESL_ENABLER_LIB = 'skyrimvresl.dll';
 
 function findGame() {
   try {
@@ -65,9 +69,23 @@ const tools = [
   },
 ];
 
+function isESLSupported(api) {
+  const state = api.getState();
+  const profileId = selectors.lastActiveProfileForGame(state, GAME_ID);
+  const discovery = selectors.discoveryByGame(state, GAME_ID);
+  if (discovery?.store === 'xbox') {
+    return false;
+  }
+  const modState = util.getSafe(state, ['persistent', 'profiles', profileId, 'modState'], {});
+  const isEnabled = (modId) => util.getSafe(modState, [modId, 'enabled'], false);
+  const mods = util.getSafe (state, ['persistent', 'mods', GAME_ID], {});
+  const hasESLEnabler = Object.keys(mods).some(modId => isEnabled(modId) && mods[modId].attributes.eslEnabler === true);
+  return hasESLEnabler;
+}
+
 function main(context) {
   context.registerGame({
-    id: 'skyrimvr',
+    id: GAME_ID,
     name: 'Skyrim VR',
     mergeMods: true,
     queryPath: findGame,
@@ -85,7 +103,43 @@ function main(context) {
     details: {
       steamAppId: 611670,
       compatibleDownloads: ['skyrimse'],
+      supportsESL: () => isESLSupported(context.api),
     }
+  });
+
+  context.once(() => {
+    context.api.onAsync('did-deploy', (profileId, newDeployment) => {
+      const state = context.api.getState();
+      const profile = selectors.profileById(state, profileId);
+      if (profile?.gameId !== GAME_ID) {
+        return Promise.resolve();
+      }
+      const discovery = selectors.discoveryByGame(state, GAME_ID);
+      if (!discovery?.path || discovery?.store === 'xbox') {
+        // Skyrim VR is currently not on Xbox, but it may be one day!
+        return Promise.resolve();
+      }
+
+      const deployedFiles = newDeployment[''];
+      const modESLEnabler = deployedFiles.find(file => file.relPath.toLowerCase().endsWith(ESL_ENABLER_LIB));
+      if (modESLEnabler === undefined) {
+        return Promise.resolve();
+      }
+
+      const mods = util.getSafe(state, ['persistent', 'mods', GAME_ID], {});
+      const mod = Object.values(mods).find(mod => mod.installationPath === modESLEnabler.source);
+      if (mod === undefined || mod.attributes.eslEnabler === true) {
+        return Promise.resolve();
+      }
+
+      const modAttributes = {
+        ...mod.attributes,
+        eslEnabler: true,
+      };
+
+      context.api.store.dispatch(actions.setModAttributes(GAME_ID, mod.id, modAttributes));
+      return Promise.resolve();
+    });
   });
 
   return true;
