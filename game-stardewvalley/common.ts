4d8773fa94158dbb4036764f0edd025dbfdfb365
IDCs <adrianraer@gmail.com>
2026-01-06 14:50:34 +0000

    fixed config mod erroneously touching smapi-internal config
    fixes nexus-mods/vortex#19197

--- game-stardewvalley/common.ts
+++ game-stardewvalley/common.ts
@@ -4,6 +4,8 @@ export const MOD_MANIFEST = 'manifest.json';
 export const RGX_INVALID_CHARS_WINDOWS = /[:/\\*?"<>|]/g;
 export const MOD_TYPE_CONFIG = 'sdv-configuration-mod';
 
+export const SMAPI_INTERNAL_DIRECTORY = 'smapi-internal';
+
 export const _SMAPI_BUNDLED_MODS = ['ErrorHandler', 'ConsoleCommands', 'SaveBackup'];
 
 export const NOTIF_ACTIVITY_CONFIG_MOD = 'sdv-config-mod-activity';
